d21eb041ec5b33e8f08b2be050fb1d00c2f85a89
aluque1 <aluquevillegas@gmail.com>
2023-04-15 15:27:23 +0200

    Iniciado codigo del cliente

---
+++ cliente_udp.c
@@ -0,0 +1,17 @@
+#define _GNU_SOURCE // added so the vscode linter doesn't bug out
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+#define BUF_SIZE 512
+
+int main(int argc, char *argv[])
+{
+    /* code */
+    return 0;
+}
